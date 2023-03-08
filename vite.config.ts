698eed2ae5f1c4ca839f3180e1ce3072aac66c91
terwer <youweics@163.com>
2023-03-08 18:26:12 +0800

    feat: Add vite and vitest

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import path from "path"
+import { defineConfig } from "vite"
+import dts from "vite-plugin-dts"
+
+export default defineConfig({
+  plugins: [dts()],
+  resolve: {
+    alias: [
+      {
+        find: "~",
+        replacement: path.resolve(__dirname, ""),
+      },
+    ],
+  },
+  build: {
+    outDir: "lib",
+    lib: {
+      entry: [path.resolve(__dirname, "src/index.ts")],
+      formats: ["es", "cjs"],
+      name: "index",
+    },
+  },
+})
