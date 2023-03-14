eadbd68cfadb0b22e0e98e4d3700826cf9462738
terwer <youweics@163.com>
2023-03-15 00:53:25 +0800

    fix: fix clean

--- script/clean.py
+++ script/clean.py
@@ -9,25 +9,8 @@ if __name__ == "__main__":
     # 切换工作空间
     scriptutils.switch_workdir()
 
-    # src
-    scriptutils.rm_files("./src/**/*.d.ts")
-    scriptutils.rm_files("./src/*.d.ts")
-
+    # clean src build
     scriptutils.rm_files("./src/**/*.map")
-    scriptutils.rm_files("./src/*.map")
-
-    scriptutils.rm_files("./src/**/*.js")
-    scriptutils.rm_files("./src/*.js")
-
-    if scriptutils.is_dir_empty("./lib/src"):
-        scriptutils.rm_folder("./lib/src")
-
-    # lib
-    scriptutils.rm_files("./lib/**/*.d.ts")
-    scriptutils.rm_files("./lib/*.d.ts")
-
-    scriptutils.rm_files("./lib/**/*.map")
-    scriptutils.rm_files("./lib/*.map")
 
     # none default dts
     scriptutils.rm_files("./typings/*alpha.d.ts")
