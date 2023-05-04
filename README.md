b596f91046994e5046f90ab5def3dec0daf0b5e3
terwer <youweics@163.com>
2023-05-04 13:37:19 +0800

    feat: init ts rollup project template

--- README.md
+++
@@ -1,6 +0,0 @@
-# {{name}}
-{{description}}
-
-## How to use
-
-For more detail, please go to [zhi-cli](https://github.com/terwer/zhi/tree/dev/apps/zhi-cli)
--- src/index.spec.ts
+++
@@ -1,8 +0,0 @@
-import { describe, expect, it } from "vitest"
-import init from "./index"
-
-describe("{{name}}", () => {
-  it("index", () => {
-    expect(init()).toBe("ok")
-  })
-})
