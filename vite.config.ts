e617864931a7f12de156363e2a14c287a0a6ae01
terwer <youweics@163.com>
2023-05-14 20:13:27 +0800

    feat: update vite template

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,11 @@
 
 import { resolve } from "path"
 import { defineConfig } from "vite"
+import dts from "vite-plugin-dts"
 
 export default defineConfig({
+  plugins: [dts()],
+
   build: {
     lib: {
       // Could also be a dictionary or array of multiple entry points
@@ -16,6 +19,9 @@ export default defineConfig({
       // make sure to externalize deps that shouldn't be bundled
       // into your library
       external: [],
+      output: {
+        entryFileNames: "[name].js",
+      },
     },
   },
 
