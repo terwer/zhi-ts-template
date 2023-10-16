ac0dcd17913a78bb705974c14b009c080679c76e
terwer <youweics@163.com>
2023-10-16 14:56:25 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
-const init = () => {
-  return "ok"
+import { simpleLogger, MainFunction } from "zhi-lib-base"
+
+/**
+ * 初始化入口
+ *
+ * @param args
+ */
+const main: MainFunction = async (args: any[]) => {
+  const logger = simpleLogger("main", "zhi", false)
+   return "ok"
 }
 
-export default init
+export default main
